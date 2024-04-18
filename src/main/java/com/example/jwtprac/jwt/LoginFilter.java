5f2b42b2e429b670a20aa847d59326f9105acf57
이희수 <hslee9234@naver.com>
2024-04-18 21:49:54 +0900

    로그인 로직 구현

--- src/main/java/com/example/jwtprac/jwt/LoginFilter.java
+++ src/main/java/com/example/jwtprac/jwt/LoginFilter.java
@@ -36,11 +36,13 @@ public class LoginFilter extends UsernamePasswordAuthenticationFilter {
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) {
 
+        System.out.println("success");
     }
 
     //로그인 실패시 실행하는 메소드
     @Override
     protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) {
 
+        System.out.println("fail");
     }
 }
